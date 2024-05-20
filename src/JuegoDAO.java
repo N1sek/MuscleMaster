6e0b605de233b279cdb266e129c0d9b5e5507614
Capaseztel <manueljesus10.mjcp@gmail.com>
2024-05-20 10:05:22 +0200

    Queries

--- src/JuegoDAO.java
+++ src/JuegoDAO.java
@@ -1,14 +1,13 @@
 import java.sql.*;
 
 public class JuegoDAO {
-    private static final String URL = "jdbc:mysql://localhost:3306/muscle_master";
+    private static final String URL = "jdbc:mysql://localhost:3306/tu_base_de_datos";
     private static final String USUARIO = "root";
-    private static final String CONTRASENIA = "admin";
+    private static final String CONTRASEÑA = "admin";
 
-    public void guardarJuego(Juego juego) {
-        try (Connection connection = DriverManager.getConnection(URL, USUARIO, CONTRASENIA)) {
+    public static void guardarJuego(Juego juego) {
+        try (Connection connection = DriverManager.getConnection(URL, USUARIO, CONTRASEÑA)) {
             // CONSULTAS AQUIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII
-
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -16,7 +15,7 @@ public class JuegoDAO {
 
     public Juego cargarJuego() {
         Juego juego = null;
-        try (Connection connection = DriverManager.getConnection(URL, USUARIO, CONTRASENIA);
+        try (Connection connection = DriverManager.getConnection(URL, USUARIO, CONTRASEÑA);
              Statement statement = connection.createStatement()) {
             // CONSULTAS AQUIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII
         } catch (SQLException e) {
